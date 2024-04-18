b13614baf953f325644a688735700c4ccef66a72
Paulo Sergio Dias <paulosvdd05@gmail.com>
2024-04-18 11:35:49 -0300

    Setando valores do objeto pessoa nos inputText

--- app/src/main/java/devandroid/dias/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/dias/applistacurso/view/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import devandroid.dias.applistacurso.R;
 import devandroid.dias.applistacurso.model.Pessoa;
@@ -11,16 +13,40 @@ import devandroid.dias.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa = new Pessoa();
+    String dadosPessoa;
+
+    EditText editPrimeiroNome;
+    EditText editSobreNome;
+    EditText editCursoDesejado;
+    EditText editTelefoneContato;
+
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editPrimeiroNome = findViewById(R.id.inputNome);
+        editSobreNome = findViewById(R.id.inputSobrenome);
+        editCursoDesejado = findViewById(R.id.inputNomeCurso);
+        editTelefoneContato = findViewById(R.id.inputTelefone);
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
         pessoa.setPrimeiroNome("Paulo");
         pessoa.setSobreNome("Dias");
         pessoa.setCursoDesejado("Android");
-        pessoa.setTelefoneContato("129942342");
-        pessoa.getPrimeiroNome();
+        pessoa.setTelefoneContato("12999999999");
+
+        editPrimeiroNome.setText(pessoa.getPrimeiroNome());
+        editSobreNome.setText(pessoa.getSobreNome());
+        editCursoDesejado.setText(pessoa.getCursoDesejado());
+        editTelefoneContato.setText(pessoa.getTelefoneContato());
+
 
         Log.i("toString", pessoa.toString());
 
