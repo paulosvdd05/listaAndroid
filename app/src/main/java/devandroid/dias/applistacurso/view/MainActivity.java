816ec2d81f1d22e41361440697c65a0de1b699f2
Paulo Sergio Dias <paulosvdd05@gmail.com>
2024-04-18 09:58:17 -0300

    Atribuindo valores ao objeto

--- app/src/main/java/devandroid/dias/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/dias/applistacurso/view/MainActivity.java
@@ -15,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pessoa.setPrimeiroNome("Paulo");
+        pessoa.setSobreNome("Dias");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("129942342");
 
 
     }
